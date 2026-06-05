2d4974bda0e701e029523f5b45fcfe70a3f84996
Aman Tyagi <amantyagi.it@gmail.com>
2026-06-05 07:13:24 +0000

    Initial SAPUI5 application

---
+++ webapp/controller/Main.controller.js
@@ -0,0 +1,60 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator"
+], (Controller, MessageToast,JSONModel, Filter, FilterOperator) => {
+    "use strict";
+
+     const App =  Controller.extend("appat.controller.Main", {
+        onInit(){
+         const viewModel = new JSONModel({
+         currency: "EUR"
+         });
+         this.getView()?.setModel(viewModel, "view");
+
+          var oSelectedModel = new JSONModel({});
+          this.getView().setModel(oSelectedModel, "selected");
+
+
+
+
+        },
+    onFilterInvoices(event) {
+    // build filter array
+      const filter = [];
+      const query = event.getParameter("query");
+      if (query) {
+        filter.push(new Filter("ProductName", FilterOperator.Contains, query));
+      }
+    // filter binding
+      const list = this.byId("ListId");
+      const binding = list?.getBinding("items");
+      binding?.filter(filter);
+    },
+    onPress() {
+       
+       var oList = this.byId("ListId");
+
+       var oSelectedItem = oList.getSelectedItem();
+           if (oSelectedItem) {
+        // 3. Get the binding context (data path and model info)
+        var oContext = oSelectedItem.getBindingContext("product");
+
+        var oData = oContext.getObject();
+
+        this.getView()
+                .getModel("selected")
+                .setData(oData);
+           
+    } else
+       {
+        sap.m.MessageToast.show("Please select an item first!");
+    }
+      }
+      
+    });
+});
+
+     
---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+QUnit.config.autostart = false;
+
+sap.ui.require(["appat/test/integration/AllJourneys"
+], function () {
+	QUnit.start();
+});
